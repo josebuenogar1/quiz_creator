009ab9158268d77ad8715066465443410bca328f
Jose Bueno <josebuenogar1@gmail.com>
2022-02-05 21:26:47 -0600

    create Quizzes model  and add to admim

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from app.models import UsersApp
+from app.models import UsersApp, Quizzes
 # Register your models here.
 admin.site.register(UsersApp)
+admin.site.register(Quizzes)
--- project/app/models.py
+++ project/app/models.py
@@ -7,3 +7,11 @@ class UsersApp(models.Model):
     
     def __str__(self):
         return str(self.name)
+
+
+class Quizzes(models.Model):
+    name=models.CharField(max_length=20, null=False)
+    user_app=models.ForeignKey(UsersApp,null=True,blank=True,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.name)
