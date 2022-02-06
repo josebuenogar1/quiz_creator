97b109a2d095299abe592a6441229ef27b8f6711
Jose Bueno <josebuenogar1@gmail.com>
2022-02-05 21:16:18 -0600

    rename model and add to admin

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class users(models.Model):
+class UsersApp(models.Model):
     name = models.CharField(max_length=50, null=False)
     password = models.CharField(max_length=200, null=False)
     
