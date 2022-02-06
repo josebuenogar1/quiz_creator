99550e3d207dd94afa5cb2c31893c7ae81bb69f9
Jose Bueno <josebuenogar1@gmail.com>
2022-02-06 11:13:46 -0600

    login, logout views

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,7 +1,11 @@
+from re import template
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView ,LogoutView
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('register/', views.register, name="register"),
+    path('login/', LoginView.as_view(template_name='app/login.html'), name="login"),
+    path('logout/',LogoutView.as_view(template_name='app/logout.html'), name="logout"),
 ]
