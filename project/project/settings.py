bb11d7e5658bfcbfd54dbcdbf53b38792042cf9f
Jose Bueno <josebuenogar1@gmail.com>
2022-02-05 20:19:01 -0600

    database created

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 PASSWOR_DB=config('PASSWORD_DB')
 USER_DB=config('USER_DB')
 DATABASES = {
