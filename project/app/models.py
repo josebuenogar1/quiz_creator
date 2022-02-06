6dc73996e26e86bf64abef685f6a8ef1e546ae2e
Jose Bueno <josebuenogar1@gmail.com>
2022-02-05 20:50:49 -0600

    users table created

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=200, null=False)
+    
+    def __str__(self):
+        return str(self.name)
