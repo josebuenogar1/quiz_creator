d998c94db375b576a46b8a169fcc19eb609eb8e6
Jose Bueno <josebuenogar1@gmail.com>
2022-02-06 14:58:27 -0600

    list quizzes by profile

--- project/app/views.py
+++ project/app/views.py
@@ -4,12 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile,Quiz
 
 # Create your views here.
 def index(request):
-    return render(request,"app/index.html")
+    context={}
+    if request.user.is_authenticated:
+        user_name=request.user.username
+        profile = Profile.objects.get(name=user_name)
+        quizzes = Quiz.objects.filter(user_app_id=profile.id)
+        context={'quizzes':quizzes}
 
+    return render(request,"app/index.html",context)
 
 def register(request):
     if request.method == 'POST':
@@ -24,7 +30,7 @@ def register(request):
     context = {'form':form}
     return render(request,'app/register.html',context)
 
-#create user and send a signal to save profile
+#create user then send a signal to save profile
 def create_profile(sender,instance,created,**kwards):
     if created:
         Profile.objects.create(name=instance)
