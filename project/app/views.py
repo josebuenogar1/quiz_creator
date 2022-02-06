d084bc6b5639fbd3875ef2afa64e78fcd9806305
Jose Bueno <josebuenogar1@gmail.com>
2022-02-06 10:58:31 -0600

    user register created

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request,"app/index.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            messages.success(request,f'User {username} created')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    context = {'form':form}
+    return render(request,'app/register.html',context)
