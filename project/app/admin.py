5a34be53ad107eb34d91de03635a1e18a5f23a6f
Jose Bueno <josebuenogar1@gmail.com>
2022-02-07 14:05:52 -0600

    question tables added

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from app.models import Profile, Quiz
+from app.models import Profile, Quiz,QuizOptions,QuizType,Questions
 # Register your models here.
+
+class QuizGetters(admin.ModelAdmin):
+    list_display = ('get_true_answer','get_false_anser_one','get_false_anser_two')
+
+
 admin.site.register(Profile)
 admin.site.register(Quiz)
+admin.site.register(QuizOptions,QuizGetters)
+admin.site.register(QuizType)
+admin.site.register(Questions)
--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,4 @@
+from msilib.schema import Class
 from django.db import models
 
 # Create your models here.
@@ -13,3 +14,38 @@ class Quiz(models.Model):
     
     def __str__(self):
         return str(self.name)
+
+class QuizOptions(models.Model):
+    sentence=models.CharField(max_length=50, null=False)
+    true_answer=models.CharField(max_length=50, null=False)
+    false_anser_one=models.CharField(max_length=50, null=False)
+    false_anser_two=models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return str(self.sentence)
+
+    @property
+    def get_true_answer(self):
+        return str(self.true_answer)
+
+    @property
+    def get_false_anser_one(self):
+        return str(self.false_anser_one)
+
+    @property
+    def get_false_anser_two(self):
+        return str(self.false_anser_two)
+
+class QuizType(models.Model):
+    name=models.CharField(max_length=20 , null=False)
+    def __str__(self):
+        return str(self.name)
+
+class Questions(models.Model):
+    number=models.IntegerField()
+    quiz_type=models.ForeignKey(QuizType,null=False,blank=False,on_delete=models.DO_NOTHING)
+    quiz=models.ForeignKey(Quiz,null=False,blank=False,on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.number)
+
+
